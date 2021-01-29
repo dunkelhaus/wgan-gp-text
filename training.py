f85bf466d95b3e582e41a25b47fdbc8a1013e3d7
dunkelhaus <jena.suraj.k@gmail.com>
2021-01-28 20:34:36 -0800

    Cleanup + docs + disabled custom gradient norm computation

--- training.py
+++ training.py
@@ -1,21 +1,51 @@
+import torch
 import imageio
 import numpy as np
-import torch
 import torch.nn as nn
-from torchvision.utils import make_grid
 from torch.autograd import Variable
+from torchvision.utils import make_grid
 from torch.autograd import grad as torch_grad
 
 
 class Trainer():
-    def __init__(self, generator, discriminator, gen_optimizer, dis_optimizer,
-                 gp_weight=10, critic_iterations=5, print_every=50,
-                 use_cuda=False):
+    """
+    Main Trainer class; provides dataset-wide train function,
+    as well as train functions per epoch, and one for each
+    the Generator and the Discriminator.
+
+    :ivar G: Generator nn.Module object.
+    :ivar D: Discriminator nn.Module object.
+    :ivar G_opt: Generator optimizer object.
+    :ivar D_opt: Discriminator optimizer object.
+    :ivar losses: Losses dictionary.
+    :ivar num_steps: Current iteration step counter.
+    :ivar use_cuda: Boolean to check whether to use cuda or gpu.
+    :ivar gp_weight: Gradient penalty weight hyperparameter.
+    :ivar critic_iterations: Number of Discriminator iterations to
+                             let pass, before updating Generator.
+    :ivar print_every: Print outputs every n iterations.
+    """
+    def __init__(
+            self,
+            generator: nn.Module,
+            discriminator: nn.Module,
+            gen_optimizer: torch.optim,
+            dis_optimizer: torch.optim,
+            gp_weight: int = 10,
+            critic_iterations: int = 5,
+            print_every: int = 50,
+            use_cuda: bool = False
+    ):
         self.G = generator
         self.G_opt = gen_optimizer
         self.D = discriminator
         self.D_opt = dis_optimizer
-        self.losses = {'G': [], 'D': [], 'GP': [], 'gradient_norm': []}
+        self.losses = {
+            'G': [],
+            'D': [],
+            'GP': [],
+            'gradient_norm': []
+        }
         self.num_steps = 0
         self.use_cuda = use_cuda
         self.gp_weight = gp_weight
@@ -26,8 +56,17 @@ class Trainer():
             self.G.cuda()
             self.D.cuda()
 
-    def _critic_train_iteration(self, data):
-        """ """
+    # XXX: Stays unchanged.
+    def _critic_train_iteration(
+            self,
+            data: torch.Tensor
+    ) -> None:
+        """
+        Run a single batch through the discriminator.
+        Computes losses, applies GP, propagates loss backward.
+
+        :param data: One batch of data from the dataloader.
+        """
         # Get generated data
         batch_size = data.size()[0]
         generated_data = self.sample_generator(batch_size)
@@ -53,8 +92,18 @@ class Trainer():
         # Record loss
         self.losses['D'].append(d_loss.data.item())
 
-    def _generator_train_iteration(self, data):
-        """ """
+    # XXX: Stays unchanged.
+    def _generator_train_iteration(
+            self,
+            data: torch.Tensor
+    ) -> None:
+        """
+        Run a single batch through the generator.
+        Computes losses using discriminator, propagates loss
+        backward.
+
+        :param data: One batch of data from the dataloader.
+        """
         self.G_opt.zero_grad()
 
         # Get generated data
@@ -70,16 +119,37 @@ class Trainer():
         # Record loss
         self.losses['G'].append(g_loss.data.item())
 
-    def _gradient_penalty(self, real_data, generated_data):
+    # XXX: Changes made.
+    # NOTE: Changed to gradnorm from gradients_norm since it looks
+    # like mentioned problem may be an issue with old pytorch.
+    # Current pytorch norm works well; if it fails,
+    # will revert to manual calculation of norm.
+    def _gradient_penalty(
+            self,
+            real_data: torch.Tensor,
+            generated_data: torch.Tensor
+    ) -> float:
+        """
+        Computes gradient penalty for given pair of real and
+        generated data.
+        Applies gradient penalty weight as well.
+
+        :param real_data: Example from training data.
+        :param generated_data: Output from Generator.
+        """
         batch_size = real_data.size()[0]
 
         # Calculate interpolation
         alpha = torch.rand(batch_size, 1, 1, 1)
         alpha = alpha.expand_as(real_data)
+
         if self.use_cuda:
             alpha = alpha.cuda()
-        interpolated = alpha * real_data.data + (1 - alpha) * generated_data.data
+
+        interpolated = (alpha * real_data.data +
+                        (1 - alpha) * generated_data.data)
         interpolated = Variable(interpolated, requires_grad=True)
+
         if self.use_cuda:
             interpolated = interpolated.cuda()
 
@@ -87,49 +157,90 @@ class Trainer():
         prob_interpolated = self.D(interpolated)
 
         # Calculate gradients of probabilities with respect to examples
-        gradients = torch_grad(outputs=prob_interpolated, inputs=interpolated,
-                               grad_outputs=torch.ones(prob_interpolated.size()).cuda() if self.use_cuda else torch.ones(
-                               prob_interpolated.size()),
-                               create_graph=True, retain_graph=True)[0]
+        gradients = torch_grad(
+            outputs=prob_interpolated,
+            inputs=interpolated,
+            grad_outputs=torch.ones(prob_interpolated.size()).cuda()
+            if self.use_cuda
+            else torch.ones(prob_interpolated.size()),
+            create_graph=True,
+            retain_graph=True
+        )[0]
 
-        # Gradients have shape (batch_size, num_channels, img_width, img_height),
+        # Gradients have shape:
+        # (batch_size, num_channels, img_width, img_height),
         # so flatten to easily take norm per example in batch
         gradients = gradients.view(batch_size, -1)
-        self.losses['gradient_norm'].append(gradients.norm(2, dim=1).mean().data.item())
+        gradnorm = gradients.norm(2, dim=1)
+        self.losses['gradient_norm'].append(
+            gradnorm.mean().data.item()
+        )
+        # print(f"Saved gradnorm:
+        #        f"{self.gp_weight * ((gradnorm - 1) ** 2).mean()}")
 
+        # --- Former issue ---
         # Derivatives of the gradient close to 0 can cause problems because of
         # the square root, so manually calculate norm and add epsilon
-        gradients_norm = torch.sqrt(torch.sum(gradients ** 2, dim=1) + 1e-12)
+        # gradients_norm = torch.sqrt(torch.sum(gradients ** 2, dim=1) + 1e-12)
+        # print(f"Used gradnorm:
+        #        f"{self.gp_weight * ((gradients_norm - 1) ** 2).mean()}")
+        # --- xxx ---
 
         # Return gradient penalty
-        return self.gp_weight * ((gradients_norm - 1) ** 2).mean()
+        return self.gp_weight * ((gradnorm - 1) ** 2).mean()
 
-    def _train_epoch(self, data_loader):
+    def _train_epoch(
+            self,
+            data_loader: torch.utils.data.DataLoader
+    ) -> None:
+        """
+        Train a single epoch of the WGAN.
+
+        :param data_loader: PyTorch training dataloader.
+        """
         for i, data in enumerate(data_loader):
             self.num_steps += 1
             self._critic_train_iteration(data[0])
-            # Only update generator every |critic_iterations| iterations
+            # Only update generator every |critic_iterations|
+            # iterations
             if self.num_steps % self.critic_iterations == 0:
                 self._generator_train_iteration(data[0])
 
             if i % self.print_every == 0:
-                print("Iteration {}".format(i + 1))
-                print("D: {}".format(self.losses['D'][-1]))
-                print("GP: {}".format(self.losses['GP'][-1]))
-                print("Gradient norm: {}".format(self.losses['gradient_norm'][-1]))
-                if self.num_steps > self.critic_iterations:
-                    print("G: {}".format(self.losses['G'][-1]))
+                print(f"Iteration {i + 1}")
+                print(f"D: {self.losses['D'][-1]}")
+                print(f"GP: {self.losses['GP'][-1]}")
+                print("Gradient norm: "
+                      f"{self.losses['gradient_norm'][-1]}")
 
-    def train(self, data_loader, epochs, save_training_gif=True):
+                if self.num_steps > self.critic_iterations:
+                    print(f"G: {self.losses['G'][-1]}")
+
+    def train(
+            self,
+            data_loader: torch.utils.data.DataLoader,
+            epochs: int,
+            save_training_gif: bool = True
+    ) -> None:
+        """
+        Topmost train function.
+
+        :param data_loader: PyTorch DataLoader for training examples.
+        :param epochs: Total number of epochs to train for.
+        :param save_training_gif: Whether to save intermediary
+                                  images during training to be
+                                  combined into one gif later.
+        """
         if save_training_gif:
             # Fix latents to see how image generation improves during training
             fixed_latents = Variable(self.G.sample_latent(64))
+
             if self.use_cuda:
                 fixed_latents = fixed_latents.cuda()
             training_progress_images = []
 
         for epoch in range(epochs):
-            print("\nEpoch {}".format(epoch + 1))
+            print(f"\nEpoch {epoch + 1}")
             self._train_epoch(data_loader)
 
             if save_training_gif:
@@ -142,17 +253,40 @@ class Trainer():
                 training_progress_images.append(img_grid)
 
         if save_training_gif:
-            imageio.mimsave('./training_{}_epochs.gif'.format(epochs),
+            imageio.mimsave(f"./training_{epochs}_epochs.gif",
                             training_progress_images)
 
-    def sample_generator(self, num_samples):
+    def sample_generator(self, num_samples: int) -> torch.Tensor:
+        """
+        Sample noise vector of shape (num_samples, latent_dim),
+        define as PyTorch Variable, and pass through the Generator.
+        Return generated data.
+
+        :param num_samples: Essentially the batch size.
+        :return generated_data: Output from Generator for sampled noise vector.
+        """
         latent_samples = Variable(self.G.sample_latent(num_samples))
+
         if self.use_cuda:
             latent_samples = latent_samples.cuda()
         generated_data = self.G(latent_samples)
+
         return generated_data
 
-    def sample(self, num_samples):
+    # XXX: Unused function.
+    def sample(self, num_samples: int) -> torch.Tensor:
+        """
+        Gets results from Trainer.sample_generator, and removes
+        the color channel before returning.
+        Note: Moves return data to cpu for numpy comp, does not
+              move it back to cuda.
+
+        :param num_samples: Essentially the batch size.
+        :return generated_data: Output from Generator for sampled
+                                noise vector, with the color
+                                channel removed.
+        """
         generated_data = self.sample_generator(num_samples)
         # Remove color channel
+
         return generated_data.data.cpu().numpy()[:, 0, :, :]
